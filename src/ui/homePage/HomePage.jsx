40c9e4133347898418a33e5ba06ff5d99cdef39d
sumon550 <entertainmentcharacter54321@gmail.com>
2026-04-17 10:39:34 +0600

    Create friend section

--- src/ui/homePage/HomePage.jsx
+++ src/ui/homePage/HomePage.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import FriendsSection from '../friendsSection/FriendsSection';
-import Banner from '../banner/Banner';
+import React from "react";
+import FriendsSection from "../friendsSection/FriendsSection";
+import Banner from "../banner/Banner";
 
 const HomePage = () => {
-    return (
-        <div>
-         <Banner/>
-            <FriendsSection/>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      {/* <FriendsSection/> */}
+    </div>
+  );
 };
 
 export default HomePage;
